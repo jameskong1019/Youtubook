6fccf4611102dbccd95006640a03f2f99401f022
jameskong1019 <jameskong1019@gmail.com>
2021-07-04 22:40:14 -0600

    Added a form, css styling and parsing url

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
-from pages.views import page
+from pages.views import page, index
 from django.urls.conf import path
 
 
 app_name = "pages"
 
 urlpatterns = [
-    path('page/', page, name='page')
+    path('', index, name='index'),
+    path('page/', page, name='page'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,22 +5,40 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from django.shortcuts import render
 import json
 import datetime
+import urllib.parse
+import validators
+import sys
+
 # fE2h3lGlOsk
-video_id = 'fE2h3lGlOsk'
+
 def page(request):
+    youtube_url = request.POST.get("youtube_url")
+    if validators.url(youtube_url) == True:
+        parsed = urllib.parse.urlparse(youtube_url)
+        video_id = urllib.parse.parse_qs(parsed.query)['v'][0]
+    else:
+        video_id = youtube_url
     pages = []
-    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
-    for transcript in transcript_list:
-        if transcript.language_code == 'ko':
-            for line in transcript.fetch():
-                jsonObject = json.loads(json.dumps(line))
-                pages.append(json.loads(json.dumps({
-                    'startTime': jsonObject["start"],
-                    'formattedTime': str(datetime.timedelta(seconds=jsonObject["start"])),
-                    'subscript': jsonObject["text"]
-                })))
-    context = {
-        'pages': pages,
-        'video_id': video_id
-    }
-    return render(request, 'page.html', context)
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript = transcript_list.find_transcript(['ko', 'en'])
+        for line in transcript.fetch():
+            jsonObject = json.loads(json.dumps(line))
+            pages.append(json.loads(json.dumps({
+                'startTime': jsonObject["start"],
+                'formattedTime': str(datetime.timedelta(seconds=jsonObject["start"])),
+                'subscript': jsonObject["text"]
+            })))
+        context = {
+            'pages': pages,
+            'video_id': video_id
+        }
+        return render(request, 'page.html', context)
+    except:
+        return render(request, 'page.html', {
+            'error_message': 'No Script Found. Please Retry Enter a url or id. Thanks:)',
+            'stack_trace': sys.exc_info()[0]
+        })
+
+def index(request):
+    return render(request, 'index.html')
